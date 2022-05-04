79d2c2244e92697f0e4af62c494c649434cd5086
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-05-03 23:07:23 -0600

    commit tree b78536d727aac2dad184bf46808aea914c94c8c6

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(books => renderBooks(books))
   
 }
 
